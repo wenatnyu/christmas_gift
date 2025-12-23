529350aae49a80371625301c8b8f3b67ab8829af
Wayne Yin <wenyin@nyu.edu>
2025-12-23 15:33:07 +0800

    1535

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  CHRISTMAS_TIME: process.env.CHRISTMAS_TIME || '2025-12-23T15:30:50+08:00',
+  CHRISTMAS_TIME: process.env.CHRISTMAS_TIME || '2025-12-23T15:35:50+08:00',
   DEV_MODE: process.env.DEV_MODE === 'true'
 };
